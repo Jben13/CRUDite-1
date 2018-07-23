9b6d8606fa4c44f5ca6084108fe5fd3b062c99b3
JBen13 <jessicabenjaminxiii@gmail.com>
2018-07-23 13:34:34 -0700

    input data from form will be stored in serialize array

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 console.log('+++ filename: app.js: jquery.ready exoected no error actual: ', $ );
 
-//attach event listener to buttons(input)
-//create function stub for read/write/delete
-$('.store-button').on('click', function(event) {
-	// event.preventDefault();
-	// event.stopPropagation(); //stops event from leaking into other listeners
-	// console.log(event.target.classList);
-	let titleValue = $('.input-title-field').val();
-    let codeValue = $('.input-code-field').val();
+// attach event listener to buttons(input)
+// create function stub for read/write/delete	
+//event.stopPropagation(); //stops event from leaking into other listeners
+// console.log(event.target.classList);
 
-	localStorage.setItem('titleValue', titleValue);
-	localStorage.setItem('codeValue', codeValue);
+$('.submit-card').on('click', function(event) {
+	event.preventDefault();
+	var data = $(':input').serializeArray();
+	  $.each(data, function(i, obj) {
+		localStorage.setItem(obj.name, obj.value);
+	  });
 
     
 });
@@ -27,31 +27,18 @@ $('.delete-button').on('click', function(event) {
 	console.log(localStorage.removeItem('booya')); //deletes given property
 });
 
-// var subject = $('.input-title-field').val();
-// var posInput = $('.pos-input').val();
-// var studyInput = $('.study-input').val();
-// var learnInput = $('.learn-input').val();
-// var recomInput = $('.recom-input').val();
-// var docInput = $('.doc-input').val();
-$(function() {
-	$('.submit-card').on ('click', function(e){
-		e.preventDefault();
-		localStorage.setItem("flag","set");
-		var data = $('#form-card').serializedArray();
-		  $.each(data, function(i, obj) {
-		  lovalStorage.setItem(obj.name, obj.value);
-		});
-	  });
-	  If (localStorage.getItem("flag")== "set"){
-		  $('.feed').before("<p>This form has saved data! </p>")
-		  var data = $('#form-card').serializedArray();
-		  $.each(data, function(i, obj) {
-			$("[name='" + obj.name + "']").val(localStorage.getItem(obj.name));
-		  
-		});
-	};
-});
 
+
+		
+		
+// 		  $.each(data, function(i, obj) {
+// 		  localStorage.setItem(obj.name, obj.value);
+// 		});
+// 	  });
+	
+// $("form").submit(function() {
+// 	$('#form-card').prependTo('.feed');
+// });
 
 
 
