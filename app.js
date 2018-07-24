dcfaa6268eabe479f28f7ba96c4bdcd486c0455f
JBen13 <jessicabenjaminxiii@gmail.com>
2018-07-23 20:35:33 -0700

    revised code

--- app.js
+++ app.js
@@ -8,20 +8,38 @@ console.log('+++ filename: app.js: jquery.ready exoected no error actual: ', $ )
 
 $('.submit-card').on("click", function(event) {
 	event.preventDefault();
-	// var $showBox = $('<div class="showBox"></div>');
 	var subject = $('.subject').val();
-	var formData = JSON.stringify(jQuery('form').serializeArray());
-	      localStorage.setItem(subject, formData);
-	//   });
-// 	  if (newList[this.name])
-		//   newList.push(data);
-	// 	  $(".showBox").append(field.name, field.value);
-	// 	  $showBox.prependTo('.feed');
+	var posText = $('#pos-input').val();
+	var studyText = $('#study-input').val();
+	var learnText = $('#learn-input').val();
+	var recomText = $('#recom-input').val();
+	var docText = $('#doc-input').val();
+	var $inputBox = $('<div class="inputBox"></div>');
+	var obj = {};
+	$('#form-card :input').not(':submit').each(function() {
+		var $input = $(this);
+		var name = $input.attr('name');
+		var value = $input.val();
+		  obj[name] = value;
+	    var newObj = JSON.stringify(obj);
+	      localStorage.setItem(subject, newObj);
+		
+	$inputBox.append(
+		subject, 
+		// + "What did I learn -OR- do well today? " + posText, 
+		// + "What do I need to study -OR- need more information? " + studyText, 
+		// + "What do I need help on? " + learnText, 
+		// + "List of Recommendations " + recomText, 
+		// + "Extra Notes: " + docText
+	);
+	$inputBox.prependTo(".feed");
+	});
 });
 
 $('.get-button').on('click', function(event) {
-	
-	$('.feed').html(`<p>${titleValue} ${codeValue}</p>`);
+	var subject = $('.subject').val();
+	localStorage.getItem('subject');
+	$('.feed').html(`<p>${subject}</p>`);
 });
 
 $('.delete-button').on('click', function(event) {
